6d87d28757addc2945ee0afc986e95a03b1daa06
Gabriel Andrade <gabriel.andrade.guilhen98@gmail.com>
2022-09-20 00:37:42 -0300

    config express e ajuste lint airbnb ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('eae');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+// eslint-disable-next-line no-console
+app.listen(3333, () => console.log('Server is running on port 3333'));
